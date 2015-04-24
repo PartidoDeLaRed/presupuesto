85db3d59aa64e6363ebf501218c7f5a9aa4a82b0
Jose Fresco <josemariafresco@gmail.com>
2015-04-24 12:13:08 -0300

    Watch task now monitors jade files

--- gulp/watch.js
+++ gulp/watch.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 
 gulp.task('watch', function () {
-  gulp.watch('lib/**/*.js', ['javascript']);
+  gulp.watch(['lib/**/*.js', 'lib/**/*.jade'], ['javascript']);
   gulp.watch('lib/**/*.styl', ['stylus']);
 });
