6a566bbfd96395c07a83237b4ecf4fd3b3931e41
Jose Fresco <josemariafresco@gmail.com>
2015-04-25 18:41:16 -0300

    Delete node_modules on clean

--- gulp/clean.js
+++ gulp/clean.js
@@ -2,5 +2,7 @@ var gulp = require('gulp');
 var rm = require('rimraf');
 
 gulp.task('clean', function (cb) {
-  rm('./public', cb);
+  rm('./public', function () {
+    rm('./node_modules', cb);
+  });
 });
