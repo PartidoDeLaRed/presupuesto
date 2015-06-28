c10211a5741887aa47279f9b2507ab2379cb470b
Guido <gvilarino@gmail.com>
2015-06-28 14:12:39 -0300

    Add fixtures for budgets

--- gulp/fixtures.js
+++ gulp/fixtures.js
@@ -2,12 +2,11 @@ import gulp from 'gulp'
 import config from '../lib/config'
 import mongoose from 'mongoose'
 
-import debug from 'debug'
-let log = debug('presupuesto:fixtures')
-
-// import fixtures
+// import fixtures and models
 import categories from '../fixtures/categories'
-let Category = require('../lib/data/models/category')
+import budgets from '../fixtures/budgets'
+import Category from '../lib/data/models/category'
+import Budget from '../lib/data/models/budget'
 
 
 gulp.task('fixtures', () => {
@@ -16,7 +15,19 @@ gulp.task('fixtures', () => {
 
   console.log('Creating fixtures in db: ', config.dbURI)
 
-  Category.create(categories).then( (saved) => {
-     return console.log('Saved %s categories to DB', saved.length)
-   })
+  Category.create(categories).then((savedCats) => {
+    console.log('Saved %s categories to DB', savedCats.length)
+
+    budgets.forEach((b) => {
+      let cat = savedCats.find((c) => {return c.name == b.name})
+      b.category = cat._id
+    })
+
+    Budget.create(budgets).then((savedBudgets) => {
+      console.log('Saved %s budgets to DB', savedBudgets.length)
+    }).then(() => {
+    mongoose.disconnect()
+    return console.log('Finished saving fixtures to DB')
+  })
+  })
 })
